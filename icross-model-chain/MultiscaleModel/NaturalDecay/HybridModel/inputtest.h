f43998929aed7922a331602a5074a185083848de
renchao_lu <renchao.lu@gmail.com>
2021-08-26 20:28:39 +0200

    [MultiscaleModel] Natural decay - hybrid model.

---
+++ icross-model-chain/MultiscaleModel/NaturalDecay/HybridModel/inputtest.h
@@ -0,0 +1,32 @@
+0.001
+100
+1
+1
+!Number of ions <=20
+1
+!temperature
+298.15
+!Surface reaction rate
+0
+!  IF OPENMP 1 yes 0 no   and  Parallel computing cores number
+0
+1
+data
+structure
+! Diffusivity
+1e-5
+! Inlet&outlet concentration （-1 mean no flux）
+1e-3
+2e-3
+! Inital concentration
+1e-3
+! Valences
+0
+!  IF MRT (1 MRT, 0 SRT)
+1
+!  IF PNP (1 PNP, 0 FICK)
+0
+!Total steps for simulation
+100
+!Number of Outputs
+1
